4253432d4d1c9752600b82cc2e6f83e8f5cd6242
yangol <olgayangol@gmail.com>
2018-05-15 19:12:17 -0400

    added presentation

--- news/news/spiders/news_spider.py
+++ news/news/spiders/news_spider.py
@@ -5,7 +5,7 @@ import re
 class NewsSpider(Spider):
     name = 'news_spider'
     #allowed_url = ['https://news.google.com/'] <-- this does not work
-    start_urls = ['https://news.google.com/gn/news/story/dzKCSGh0R5pOluMQHVnkCkTl0eZMM?hl=en&ned=us&gl=US']
+    start_urls = ['https://news.google.com/gn/news/story/d-YmY5NnVXRPlKMV621fjDwvdRvdM?hl=en&ned=us&gl=US']
     #start_urls=['https://www.google.com/search?q=turkey+presidential+elections&tbm=nws&source=lnt&tbs=qdr:d&sa=X&ved=0ahUKEwikrdeS4f3aAhVNx1kKHcOxDg8QpwUIHg&biw=1440&bih=704&dpr=1']
     
     #def parse_through_pages(self, response):
@@ -18,13 +18,13 @@ class NewsSpider(Spider):
 
     def parse(self, response):
 
-        total=len(response.xpath('//section[4]/c-wiz[*]/c-wiz/a/text()'))
+        total=len(response.xpath('//section[5]/c-wiz[*]/c-wiz/a/text()'))
 
         #total=len(response.xpath('//div[*]/div/div/h3/a/text()'))
 
         for i in range(1,total+1):
-            headline_path='//section[4]/c-wiz['+ str(i) +']/c-wiz/a/text()'
-            source_path='//section[4]/c-wiz[' + str(i) +']//div/span[1]/text()'
+            headline_path='//section[5]/c-wiz['+ str(i) +']/c-wiz/a/text()'
+            source_path='//section[5]/c-wiz[' + str(i) +']//div/span[1]/text()'
 
             #//div[*]//div/h3/a
             #headline_path='//div[' +str(i) + ']//div/h3/a'
